8c161a76cad6f97ac3954f7b0065078b8bfc1eb9
Melanie Mohn <melaniemohn@yahoo.com>
2017-07-25 18:23:42 -0400

    add temporary favicon

---
+++ project-data.js
@@ -0,0 +1,37 @@
+
+// add images to each of these
+// also, certain projects, like align and times two, will have "watch it in action"... link to youtube?
+
+/* BASIC FORMAT
+const PROJECT = {
+	name: xxx,
+	image: xxx,
+	tagline: xxx,
+	description: xxx,
+	deployedUrl: xxx,
+	github: xxx
+}
+*/
+
+const align = {
+	name: 'Align',
+	deployedUrl: 'align.fun'
+};
+
+const timestwo = {
+	name: 'Times Two'
+};
+
+const techtalk = {
+	name: 'Tech Talk'
+};
+
+const graceshopper = {
+	name: 'Bits & Bytes'
+};
+
+// add contact info for icons, too?
+
+const projects = [align, timestwo, techtalk, graceshopper];
+
+module.exports = {projects};
