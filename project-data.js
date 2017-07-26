82a748e07bcdc5aabd4ad65617ed631c4d74be54
Melanie Mohn <melaniemohn@yahoo.com>
2017-07-26 16:28:01 -0400

    update deployed site

--- project-data.js
+++ project-data.js
@@ -26,12 +26,21 @@ const techtalk = {
 	name: 'Tech Talk'
 };
 
-const graceshopper = {
+const bitsbytes = {
 	name: 'Bits & Bytes'
 };
 
 // add contact info for icons, too?
+/*
+const twitterIcon = {
+	label: 'Twitter',
+	class: 'icon fa-twitter',
+	href: 'https://twitter.com/melaniemohn',
+	hover: '#1c94e0'
+};
+*/
 
-const projects = [align, timestwo, techtalk, graceshopper];
+// const socialIcons = [twitterIcon];
+const projects = [align, timestwo, techtalk, bitsbytes];
 
-module.exports = {projects};
+module.exports = {socialIcons, projects};
