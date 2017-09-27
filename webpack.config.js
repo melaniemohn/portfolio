a60183e5ab27aa9362156a84b857a5ae89454d3c
Melanie Mohn <melaniemohn@yahoo.com>
2017-09-27 12:49:45 -0400

    fixed build script FINALLY

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,22 @@
 'use strict';
 
-const LiveReloadPlugin = require('webpack-livereload-plugin')
-    , devMode = require('.').isDevelopment
-    , USE_FAST_SOURCE_MAPS = false
-
 module.exports = {
-  entry: './app/main.jsx',
+  entry: './index.jsx',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
-  context: __dirname,
-  devtool: devMode && USE_FAST_SOURCE_MAPS
-    ? 'cheap-module-eval-source-map'
-    : 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '*']
   },
   module: {
     rules: [{
       test: /jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'es2015'] // MPM: removed 'stage-2'
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+          presets: ['react']
         }
       }]
-    }]
-  },
-  plugins: devMode
-    ? [new LiveReloadPlugin({appendScriptTag: true})]
-    : []
-};
+    }
+  };
