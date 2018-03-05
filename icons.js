ed11113da224ce240bf659d972d25ac06191cf82
Melanie Mohn <melaniemohn@yahoo.com>
2018-03-04 22:04:15 -0500

    add angellist link ✌🏼

--- icons.js
+++ icons.js
@@ -1,10 +1,10 @@
-// TODO: add angellist icon
 
 const GitHub = {id: 'github', url: 'https://github.com/melaniemohn', icon: 'fa fa-github', style: {color: '#333'}};
-const Twitter = {id: 'twitter', url: 'https://www.twitter.com/melaniemohn', icon: 'fa fa-twitter-square', style: {color: '#55acee'}};
-const LinkedIn = {id: 'linkedin', url: 'https://www.linkedin.com/in/melanie-mohn', icon: 'fa fa-linkedin-square', style: {color: '#0976b4'}};
+const Twitter = {id: 'twitter', url: 'https://www.twitter.com/melaniemohn', icon: 'fa fa-twitter', style: {color: '#55acee'}};
+const LinkedIn = {id: 'linkedin', url: 'https://www.linkedin.com/in/melanie-mohn', icon: 'fa fa-linkedin', style: {color: '#0976b4'}};
+const AngelList = {id: 'angellist', url: 'https://angel.co/melanie-mohn', icon: 'fa fa-angellist', style: {color: 'black'}};
 const Mail = {id: 'mail', url: 'mailto:melaniemohn@gmail.com?Subject=Hello', icon: 'fa fa-envelope-square', style: {color: '#dd4b39'}};
 
-const Icons = [GitHub, Twitter, LinkedIn, Mail];
+const Icons = [GitHub, Twitter, LinkedIn, AngelList, Mail];
 
 module.exports = Icons;
