5ed8e9b54572bdaf74dc5533aaada9ce9dd638b8
Melanie Mohn <melaniemohn@yahoo.com>
2018-03-06 00:12:37 -0500

    reorder elements on about me page and add icons to skills list. TODO: grid layout.

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -5,49 +5,50 @@ import React from 'react';
 
 export default function Skills() {
 	return (
-		<div className="skills-container">
-			<h3>skills</h3>
+		<div>
 			<p>Recently, my focus has been on JavaScript across the stack. It's my priority to write clean and performant code, but I also love to experiment. I'm always eager to try new flavors of JavaScript (read: front-end frameworks), but I'm also eager to learn new back-end languages, too.</p>
 
-			<h4 className="skills-list-header">Front-end tools:</h4>
-			<ul className="skills-list">
-				<li>JavaScript</li>
-				<li>HTML5</li>
-				<li>CSS3</li>
-				<li>Sass</li>
-				<li>jQuery</li>
-			</ul>
+			<h3>skills</h3>
+			<div className="skills-container">
+				<h4 className="skills-list-header">Front-end tools:</h4>
+				<ul className="skills-list">
+					<li>JavaScript</li>
+					<li>HTML5</li>
+					<li>CSS3</li>
+					<li>Sass</li>
+					<li>jQuery</li>
+				</ul>
 
-			<h4 className="skills-list-header">Libraries and frameworks:</h4>
-			<ul className="skills-list">
-				<li>React</li>
-				<li>Redux</li>
-				<li>D3.js</li>
-				<li>Bootstrap</li>
-				<li>Material UI</li>
-			</ul>
+				<h4 className="skills-list-header">Libraries and frameworks:</h4>
+				<ul className="skills-list">
+					<li>React</li>
+					<li>Redux</li>
+					<li>D3.js</li>
+					<li>Bootstrap</li>
+					<li>Material UI</li>
+				</ul>
 
-			<h4 className="skills-list-header">Back-end:</h4>
-			<ul className="skills-list">
-				<li>Node.js</li>
-				<li>Express</li>
-				<li>SQL (MySQL, PostgreSQL)</li>
-				<li>Sequelize</li>
-				<li>Firebase</li>
-				<li>OrientDB</li>
-				<li>Java</li>
-			</ul>
+				<h4 className="skills-list-header">Back-end:</h4>
+				<ul className="skills-list">
+					<li>Node.js</li>
+					<li>Express</li>
+					<li>SQL (MySQL, PostgreSQL)</li>
+					<li>Sequelize</li>
+					<li>Firebase</li>
+					<li>OrientDB</li>
+					<li>Java</li>
+				</ul>
 
-			<h4 className="skills-list-header">Development practices:</h4>
-			<ul className="skills-list">
-				<li>Git</li>
-				<li>Jira</li>
-				<li>Agile</li>
-				<li>Test-driven development</li>
-				<li>Cucumber</li>
-				<li>Selenium</li>
-			</ul>
-
+				<h4 className="skills-list-header">Development practices:</h4>
+				<ul className="skills-list">
+					<li>Git</li>
+					<li>Jira</li>
+					<li>Agile</li>
+					<li>Test-driven development</li>
+					<li>Cucumber</li>
+					<li>Selenium</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
--- pages/About.jsx
+++ pages/About.jsx
@@ -11,9 +11,8 @@ export default function About() {
 	return (
 		<div>
 			<h1>about me</h1>
-			<p>Here's a placeholder for a longer "about me" section.</p>
-			<Link to="/resume">Check out my resume</Link> or <Link to="/">go back to the main page.</Link>
 			<Skills />
+			<Link to="/resume">Check out my resume</Link> or <Link to="/">go back to the main page.</Link>
 		</div>
 	);
 }
