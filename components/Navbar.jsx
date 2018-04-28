ca61281537fc43533d3145fffecbce4b39eda15f
Melanie Mohn <melaniemohn@yahoo.com>
2018-04-28 09:57:19 -0400

    rewriting right nav component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,20 +6,12 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
 	return (
 		<nav>
-			<ul className="nav-list">
-				<li>
-					<Link className="nav-item" to="/contact">reach out</Link>
-				</li>
-				<li>
-					<Link className="nav-item" to="/resume">resume</Link>
-				</li>
-				<li>
-					<Link className="nav-item" to="/projects">projects</Link>
-				</li>
-				<li>
-					<Link className="nav-item" to="/about">about me</Link>
-				</li>
-			</ul>
+			<div className="nav-right">
+				<Link className="nav-item" to="/about">about me</Link>
+				<Link className="nav-item" to="/projects">projects</Link>
+				<Link className="nav-item" to="/resume">resume</Link>
+				<Link className="nav-item" to="/contact">reach out</Link>
+			</div>
 		</nav>
 	);
 }
